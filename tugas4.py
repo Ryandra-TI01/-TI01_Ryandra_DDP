0a56db81f930121ccfbba778000a810818908dfb
Ryandra-TI01 <ryandra.athaya1705@gmail.com>
2023-10-25 20:55:10 +0700

    abis diedit

--- tugas4.py
+++ tugas4.py
@@ -6,22 +6,22 @@ var1.insert(2,"kawasaki")
 print(var1)
 
 # soal nomor 2
-# pilihan1 = input("masukan luas yang diingikan \npersegi ketik 1\nlingkaran ketik 2\nsegitiga ketik 3\nmasukan angka : ")
+pilihan1 = input("masukan luas yang diingikan \npersegi ketik 1\nlingkaran ketik 2\nsegitiga ketik 3\nmasukan angka : ")
 
-# match pilihan1:
-#     case "1":
-#         a = int(input("masukan sisi: "))
-#         luasp = a*a
-#         print(luasp)
-#     case "2":
-#         jari_jari = int(input("masukan jari-jari: "))
-#         luasl = 3.14*jari_jari
-#         print(luasl)
-#     case "3":
-#         a = int(input("masukan alas: "))
-#         t = int(input("masukan tinggi: "))
-#         luass = 1/2*a*t
-#         print(luass)
-#     case _:
-#         print("oprasi tidak ditemukan")
+match pilihan1:
+    case "1":
+        a = int(input("masukan sisi: "))
+        luasp = a*a
+        print(luasp)
+    case "2":
+        jari_jari = int(input("masukan jari-jari: "))
+        luasl = 3.14*jari_jari
+        print(luasl)
+    case "3":
+        a = int(input("masukan alas: "))
+        t = int(input("masukan tinggi: "))
+        luass = 1/2*a*t
+        print(luass)
+    case _:
+        print("oprasi tidak ditemukan")
         
