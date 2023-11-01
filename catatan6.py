bc1ae94b5824f07884f46a963ab0b9210fcd0c93
Ryandra-TI01 <ryandra.athaya1705@gmail.com>
2023-11-01 10:53:39 +0700

    Tugas 6

---
+++ catatan6.py
@@ -0,0 +1,24 @@
+# a=99
+# while a>=1:
+#     print("stt nurul fikri",a)
+#     a-=2
+# j=1
+# while(j<=10):
+#     k=1
+#     while(k<=10):
+#         print(j*k,end="")
+#         k+=1
+#     print()
+#     j+=1
+
+# for i in range(2,10,2):
+#     print(i)
+# for i in range(30,10,-2):
+#     print(i)
+# for i in range(2,10,2):
+#     print("apa")
+for i in range(1,5):
+    for j in range(1,5):
+        k=i*j
+        print(k,end='')
+    print()
